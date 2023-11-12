17de7f43b1c48dc8fdb9386225d92d6f3996af65
e4e6 <hangyeol460@gmail.com>
2023-11-12 14:57:23 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var figlet = require("figlet");
+
+figlet.text(
+    "node.",
+    {
+      font: "Ghost",
+    //   horizontalLayout: "default",
+    //   verticalLayout: "default",
+      width: 100,
+      whitespaceBreak: true,
+    },
+    function (err, data) {
+      if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+      }
+      console.log(data);
+    }
+  );
